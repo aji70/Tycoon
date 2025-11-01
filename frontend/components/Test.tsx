dc751eb71480a36529b3c40be049e3871fbfd209
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-11-01 20:46:54 +0100

    project init

--- frontend/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Test = () => {
-  return (
-    <div className='flex-1 min-h-screen items-center justify-center text-[20px] text-[#000]'>Test</div>
-  )
-}
-
-export default Test
--- frontend/types/dojoengine-sdk.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@dojoengine/sdk' {
-  export type SchemaType = any;
-}
