c4ddedcaace6d5099bd2345282d3beca3b39240f
aji70 <realjaiboi70@gmail.com>
2026-02-27 23:24:21 +0100

    tournamen escrow building on stacks

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,6 +29,8 @@ export async function generateMetadata(): Promise<Metadata> {
     description:
       "Tycoon is a decentralized on-chain game inspired by the classic Monopoly game, built on Celo. It allows players to buy, sell, and trade digital properties in a trustless gaming environment.",
     other: {
+      "talentapp:project_verification":
+        "5d078ddf22e877e4b4a4508b55b82c826e0b7d2bef4d1505b4b14945a216f62eaf013de3c9fe99c4fd58ae7fc896455a9ada31130565d32c8a5eb785b394113a",
       "base:app_id": "695d328c3ee38216e9af4359", 
       "fc:frame": JSON.stringify({
         version: minikitConfig.miniapp.version,
