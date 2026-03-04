846ded28a25502085f716ef6f0d9d923a3f2dd87
aji70 <realjaiboi70@gmail.com>
2026-03-05 00:17:50 +0100

    cleanup 2

--- frontend/types/index.d.ts
+++ frontend/types/index.d.ts
@@ -1,11 +1,6 @@
 import React from "react";
 import { StaticImageData } from "next/image";
 
-export type MaxWrapperTypes = {
-  children: React.ReactNode;
-  className?: string;
-};
-
 export type LogoTypes = {
   className: string;
   image: StaticImageData;
