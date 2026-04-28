55467b15f401a459a47effc4f5792f84530a1aac
aji70 <realjaiboi70@gmail.com>
2026-04-28 20:47:49 +0100

    update

--- frontend/context/ContractProvider.tsx
+++ frontend/context/ContractProvider.tsx
@@ -8,7 +8,7 @@ import {
   useAccount,
   useWaitForTransactionReceipt,
   useChainId,
-  usePublicClien
+  usePublicClient,
 } from 'wagmi';
 import { celo } from 'wagmi/chains';
 import { Address, createPublicClient, decodeEventLog, getAddress, hexToBigInt, http, parseEventLogs, zeroAddress } from 'viem';
