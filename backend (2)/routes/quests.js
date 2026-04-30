426da3936bc2aa917d303af2994461aa20529416
aji70 <realjaiboi70@gmail.com>
2026-04-30 22:11:36 +0100

    minipay v1

--- backend (2)/routes/quests.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as questsPublicController from "../controllers/questsPublicController.js";
-
-const router = express.Router();
-
-router.get("/", questsPublicController.listPublicQuests);
-
-export default router;
--- frontend (2)/app/tournaments/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { TournamentDetailPage } from "@/components/tournaments/TournamentDetailPage";
-
-export default function HumanTournamentDetailPage() {
-  return <TournamentDetailPage basePath="/tournaments" expectedKind="human" />;
-}
